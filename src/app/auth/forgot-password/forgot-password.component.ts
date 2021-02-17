e9b3c90a105e3930cfb40322c814ec2ee4908b05
pminkevich <pminkevich@gmail.com>
2021-02-17 13:40:54 -0300

    add forgot password

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
-export class ForgotPasswordComponent implements OnInit {
+export class ForgotPasswordComponent   {
 
-  constructor() { }
+ 
+  userEmail = new FormControl('');
+  constructor(private authSvc: AuthService, private router: Router) { }
 
-  ngOnInit(): void {
+  async onReset() {
+    try {
+      const email = this.userEmail.value;
+      await this.authSvc.resetPassword(email);
+      window.alert('Se Ha enviado Un Email. Verifique su Bandeja de Entrada!');
+      this.router.navigate(['/login']);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
